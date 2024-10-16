45f8efe89476359e171332d4987de4eb28120253
Zé Matheus <josemw86@gmail.com>
2024-10-16 00:50:24 -0300

    project creation

---
+++ src/test/java/com/matheus/reactjava/ReactJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.reactjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
